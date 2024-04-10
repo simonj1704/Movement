f783ee0275d69b930c969434eea337633e131435
simonj1704 <simo915m@stud.kea.dk>
2024-04-10 20:24:14 +0200

    Add controls object to track player movement in controller.js

--- controller.js
+++ controller.js
@@ -13,6 +13,13 @@ const player = {
     y: 0
 }
 
+const controls = {
+    up: false,
+    down: false,
+    left: false,
+    right: false
+}
+
 function displayPlayerposition(){
     const visualPlayer = document.querySelector("#player");
     visualPlayer.style.translate = `${player.x}px ${player.y}px`;
